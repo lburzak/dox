8cdbf7347dff9b77171d102a233a943b52dd6a64
polydome <polydome@outlook.com>
2021-03-15 21:27:53 +0100

    Implement drag and drop

--- script/main.js
+++ script/main.js
@@ -1,3 +1,54 @@
-$('#sidebar').resizable({
-  handles: 'e'
-});
+class ScratchRepository {
+    data = [
+        { id: 0, content: "This is some scratch text" },
+        { id: 1, content: "Lorem ipsum dolor sit amet, consectetur adipiscing."},
+        { id: 2, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet imperdiet nisi, ut\n" +
+                "    hendrerit magna.\n" },
+        { id: 3, content: "Hello" }
+    ];
+
+    findAll() {
+        return [ ...this.data ];
+    }
+
+    findOne(id) {
+        return this.data.find(scratch => scratch.id === id);
+    }
+
+    removeOne(id) {
+        this.data = this.data.filter(scratch => scratch.id !== id);
+    }
+}
+
+function createScratch(scratch) {
+    return $("<div/>", {
+        class: "card",
+        html: scratch.content
+    }).draggable({
+        revert: true
+    }).data("id", scratch.id)
+}
+
+$(document).ready(function () {
+    const scratchRepository = new ScratchRepository();
+
+    scratchRepository.findAll().forEach(scratch => {
+        createScratch(scratch).appendTo("#sidebar")
+    })
+
+    $('#sidebar').resizable({
+        handles: 'e'
+    });
+
+    $('#canvas-input').droppable({
+        drop: function (event, ui) {
+            const id = ui.draggable.data("id")
+            const scratch = scratchRepository.findOne(id)
+
+            if (scratch !== undefined) {
+                scratchRepository.removeOne(id)
+                $(this).append(scratch.content);
+            }
+        }
+    })
+})
