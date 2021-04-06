6c2acc3ea1ca92ac7a1f7d03616da038cb233609
polydome <polydome@outlook.com>
2021-04-06 09:36:21 +0200

    Refactor rename styles

--- script/main.js
+++ script/main.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     const scratchRepository = new ScratchRepository(localStorage);
     const docRepository = new DocRepository(localStorage);
 
-    const inputBoxController = new InputBoxController($('#scratch-input'));
-    const listController = new RepositoryListController($('#items-list'));
-    new SidebarController($('#files-nav'), docRepository, scratchRepository, inputBoxController, listController);
-    new EditorController($('#canvas-input'), scratchRepository);
+    const inputBoxController = new InputBoxController($('#sidebar-input-box'));
+    const listController = new RepositoryListController($('#sidebar-list'));
+    new SidebarController($('#sidebar-tabs'), docRepository, scratchRepository, inputBoxController, listController);
+    new EditorController($('#editor textarea'), scratchRepository);
 
     $('#sidebar').resizable({handles: 'e'});
 })
